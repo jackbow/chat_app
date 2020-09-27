251eb6bc72e1cc779461b32cf67fa4ec71cc876f
Jack Bowman <jackbow@umich.edu>
2020-09-26 21:14:51 -0700

    fix path errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/tailwind.css';
+import './tailwind.css';
 import App from './App';
 import * as serviceWorker from './utilities/serviceWorker';
 
